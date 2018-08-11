3b86c003f5c7768bab4b22a3e82a7cfc2c74e9b6
larrychina <963198shang>
2018-08-11 15:33:25 +0800

    log test

--- springboot05/src/test/java/org/larry/log/springboot05/Springboot05ApplicationTests.java
+++ springboot05/src/test/java/org/larry/log/springboot05/Springboot05ApplicationTests.java
@@ -2,6 +2,8 @@ package org.larry.log.springboot05;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +11,17 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class Springboot05ApplicationTests {
 
+    Logger logger = LoggerFactory.getLogger(this.getClass()) ;
     @Test
     public void contextLoads() {
+        logger.info("****************************info***************************************");
+        logger.debug("****************************debug***************************************");
+        logger.error("****************************error***************************************");
+        System.out.println("hello");
+        logger.info("****************************info***************************************");
+        logger.debug("****************************debug***************************************");
+        logger.error("****************************error***************************************");
+
     }
 
 }
